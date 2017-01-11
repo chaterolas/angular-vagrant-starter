84212340d0fd1c39f2dbe46108625c98216435a0
Alaín Chevanier <alain.perez.consultant@nielsen.com>
2017-01-10 18:46:04 -0600

    Beta configuración vagrant

--- dependencies/bootstrap.sh
+++ dependencies/bootstrap.sh
@@ -2,54 +2,56 @@
 
 # Update & Install
 apt-get update
-apt-get install -y build-essential git curl
-
-# Reading Environment Varaibles
-#echo "Reading environment variables..."
-# Check if env/ directory exists
-#if [ -d /vagrant/env/ ]; then
-  #for path in /vagrant/env/*; do
-    #name=${path##*/}
-    # Do not include dotfiles or empty directory (*)
-    #if [[ "$name" != "*" ]] && [[ ${name:0:1} != "." ]]; then
-      #echo "$name=$(cat $path)"
-      #echo "export $name=$(cat $path)" >> /home/vagrant/.bashrc
-    #fi
-  #done
-#fi
-
-# Setting Environment Varaibles
-echo "Setting environment variables..."
-echo "export NODE_ENV=development" >> /home/vagrant/.bashrc
-#echo "\ncd /vagrant" >> /home/vagrant/.bashrc
+apt-get install -y build-essential git curl emacs
 
 export HOME=/home/vagrant
+
 
 # Installing nvm
-echo "Installing nvm..."
+echo "INSTALLING NVM..."
 curl https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
-#echo "source ~/.nvm/nvm.sh" >> /home/vagrant/.bashrc
-#source /home/vagrant/.nvm/nvm.sh
+echo "source $HOME/.nvm/nvm.sh" >> $HOME/.bashrc
+source $HOME/.nvm/nvm.sh
+echo "DONE INSTALLING NVM"
+
 
 # Installing Node.JS
-echo "Installing Node.JS..."
+echo "INSTALLING NODEJS..."
 nvm install 7.4.0
-chown -R vagrant:vagrant /home/vagrant/.nvm
-export HOME=/home/vagrant
-
-
-# Installing rbenv
-
-git clone https://github.com/rbenv/rbenv.git /home/vagrant/.rbenv
-cd /home/vagrant/.rbenv && src/configure && make -C src
-
-PATH="$PATH:$HOME/.rbenv/bin"
-echo "export $PATH" >> $HOME/.bashrc
-
-
-# echo "PATH=$PATH:/vagrant/node_modules/.bin" >> /home/vagrant/.bashrc
+chown -R vagrant:vagrant $HOME/.nvm
+echo "DONE INSTALLING NODE"
 
 
 # Installing NPM packages
-# echo "Installing NPM packages..."
-# npm install -g grunt-cli bower yo generator-karma generator-angular
+echo "INSTALLING NPM PACKAGES..."
+npm install -g grunt-cli bower yo generator-karma generator-angular
+echo "DONE INSTALLING NPM PACKAGES"
+
+
+# Installing rbenv
+echo "INSTALLING RBENV..."
+git clone https://github.com/rbenv/rbenv.git $HOME/.rbenv
+cd $HOME/.rbenv && src/configure && make -C src
+echo "export PATH=$HOME/.rbenv/bin:$PATH" >> $HOME/.bashrc
+echo 'eval "$(rbenv init -)"' >> $HOME/.bashrc
+chown -R vagrant:vagrant $HOME/.rbenv
+echo "DONE INSTALLING RBENV"
+exec $SHELL
+
+export HOME=/home/vagrant
+
+# Installing ruby build
+echo "INSTALLING RUBY_BUILD..."
+git clone git://github.com/sstephenson/ruby-build.git $HOME/.rbenv/plugins/ruby-build
+echo "export PATH=$HOME/.rbenv/plugins/ruby-build/bin:$PATH" >> $HOME/.bashrc
+chown -R vagrant:vagrant $HOME/.rbenv/plugins
+echo "DONE INSTALLING RUBY BUILD"
+exec $SHELL
+
+
+# Installing Ruby
+echo "INSTALLING RUBY..."
+rbenv install 2.3.0
+rbenv global 2.3.0
+rbenv rehash
+echo "DONE INSTALLING RUBY"
